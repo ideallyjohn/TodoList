07e4193cd047d32888cc3c863c1eb23be674ee74
ideallyjohn <johnwelte@gmail.com>
2023-03-01 07:59:09 -0500

    Update

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 
 // Serve static files from the public directory
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
 
-// Serve the built version of the React app from the client/build directory
+// Serve the built version of the React app from the todo-list/build directory
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Serve the index.html file for any other requests
@@ -22,13 +22,29 @@ app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
 
-app.get('/api/tasks', (req, res) => {
+app.get('/api/todos', (req, res) => {
   // In a real app, you would retrieve the tasks from a database
   const tasks = [
-    { id: 1, text: 'Task 1', completed: false },
-    { id: 2, text: 'Task 2', completed: true },
-    { id: 3, text: 'Task 3', completed: false },
+    { id: 1, text: 'Todo 1', completed: false },
+    { id: 2, text: 'Todo 2', completed: true },
+    { id: 3, text: 'Todo 3', completed: false },
   ];
 
-  res.json(tasks);
+  res.json(todos);
+});
+
+const todos = [];
+
+app.post('/api/tasks', (req, res) => {
+  const newTodo = {
+    id: Date.now(),
+    text: req.body.text,
+    completed: req.body.completed || false,
+  };
+
+  console.log(`New todo added: ${newTodo.text}`); // Log the text of the new todo
+
+  todos.push(newTodo);
+
+  res.json(newTodo);
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,88 +1,95 @@
 import React, { useState } from 'react';
+import { ResizableBox } from 'react-resizable';
+import 'react-resizable/css/styles.css';
+import Draggable from 'react-draggable';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
-  // Define state variables for the todo list and the input value
-  const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState([]);
 
-  // Define a function to handle adding a new todo item
   const handleAddTodo = () => {
-    if (inputValue.trim() === '') {
-      return;
+    if (inputValue) {
+      const newTodo = {
+        id: todos.length + 1,
+        text: inputValue,
+        completed: false,
+      };
+      setTodos([...todos, newTodo]);
+      setInputValue('');
     }
-
-    const newTodo = {
-      id: Date.now(),
-      text: inputValue,
-      completed: false,
-    };
-
-    setTodos([...todos, newTodo]);
-    setInputValue('');
   };
 
-  // Define a function to handle toggling the completed status of a todo item
   const handleToggleTodo = (id) => {
     const updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
-        return {
-          ...todo,
-          completed: !todo.completed,
-        };
-      } else {
-        return todo;
+        todo.completed = !todo.completed;
       }
+      return todo;
     });
-
     setTodos(updatedTodos);
   };
 
-  // Define a function to handle removing a todo item
   const handleRemoveTodo = (id) => {
     const updatedTodos = todos.filter((todo) => todo.id !== id);
     setTodos(updatedTodos);
   };
 
   return (
-    <div className="app">
-      <div className="app__header">
-        <FontAwesomeIcon icon={faCheck} className="app__header-icon" />
-        <h1 className="app__title">Todo List</h1>
-      </div>
-      <div className="app__form">
-        <input
-          className="app__form-input"
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <button className="app__form-button" onClick={handleAddTodo}>
-          Add
-        </button>
-      </div>
-      <ul className="app__list">
-        {todos.map((todo) => (
-          <li className="app__list-item" key={todo.id}>
-            <span
-              className="app__list-text"
-              style={{
-                textDecoration: todo.completed ? 'line-through' : 'none',
-              }}
-              onClick={() => handleToggleTodo(todo.id)}
-            >
-              {todo.text}
-            </span>
-            <button
-              className="app__list-delete-button"
-              onClick={() => handleRemoveTodo(todo.id)}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
+    <div className="app-container">
+      <Draggable handle=".react-resizable-handle">
+        <ResizableBox
+          width={500}
+          height={400}
+          minConstraints={[300, 200]}
+          style={{ position: 'relative' }}
+        >
+          <div className="app">
+            <div className="handle react-resizable-handle"></div>
+            <div className="container">
+              <div className="app__header">
+                <FontAwesomeIcon icon={faCheck} className="app__header-icon" />
+                <h1 className="app__title">Todo List</h1>
+              </div>
+              <div className="app__form">
+                <input
+                  className="app__form-input"
+                  type="text"
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                />
+                <button className="app__form-button" onClick={handleAddTodo}>
+                  Add
+                </button>
+              </div>
+              <ul className="app__list">
+                {todos.map((todo) => (
+                  <li className="app__list-item" key={todo.id}>
+                    <span
+                      className="app__list-text"
+                      style={{
+                        textDecoration: todo.completed
+                          ? 'line-through'
+                          : 'none',
+                      }}
+                      onClick={() => handleToggleTodo(todo.id)}
+                    >
+                      {todo.text}
+                    </span>
+                    <button
+                      className="app__list-delete-button"
+                      onClick={() => handleRemoveTodo(todo.id)}
+                    >
+                      Delete
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </ResizableBox>
+      </Draggable>
     </div>
   );
 }
